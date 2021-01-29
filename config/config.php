4e7fbf24a2f686bdf2fa2e2c1a7c60614429e781
Carlos Hurtado <gabo_duty@hotmail.com>
2021-01-29 16:59:55 -0500

    agergado codigo de ubicacion actual

--- config/config.php
+++ config/config.php
@@ -1,3 +1,10 @@
 <?php
 
 define("_DOMAIN", "http://localhost/PruebaAlejo/alejo/ITSYS.SAS/");
+
+$ubicacionAcxtual = "RAIZ DENTRO CARPETA";
+
+"$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; //Obtener ruta actual
+//if()
+//
+//define("_DOMAINLIBRERIAS", "");
